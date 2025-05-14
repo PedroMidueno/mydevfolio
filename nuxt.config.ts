4b0ba4b4db68820a6dff3ddb2e3f3ae84fc8e997
PedroMidueno <pedromidueno.dev@gmail.com>
2025-05-13 22:07:20 -0600

    config: update project config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
+import { dark } from "@clerk/themes";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      bucketPrefixUrl: process.env.R2_BUCKET_PUBLIC_URL
+    }
+  },
   modules: ['@nuxt/eslint', '@nuxt/ui', '@prisma/nuxt', '@clerk/nuxt'],
   css: ['@/assets/css/main.css'],
   eslint: {
@@ -10,7 +17,15 @@ export default defineNuxtConfig({
     }
   },
   ui: {
-    prefix: 'nui',
-    colorMode: false
+    prefix: 'nui'
+  },
+  colorMode: {
+    preference: 'dark',
+    fallback: 'dark',
+  },
+  clerk: {
+    appearance: {
+      baseTheme: dark
+    }
   }
 })
