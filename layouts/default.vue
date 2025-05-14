a374b35f5b52f0d9cedae2a37acdb25ba0bbea25
PedroMidueno <pedromidueno.dev@gmail.com>
2025-05-14 12:16:04 -0600

    feat: update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,28 @@
+<script setup lang="ts">
+const hideSignInButton = createRouteMatcher(['/sign-in', '/sign-up'])
+const route = useRoute()
+</script>
+
 <template>
-  <div>
-    <header>
-      MyDevFolio Layout
+  <section class="max-w-[1400px] mx-auto min-h-screen relative">
+    <header class="flex justify-between h-16 items-center px-4 bg-blue-800 sticky top-0 z-10 rounded-sm">
+      <div>
+        <u-link class="text-xl font-bold text-gray-50" to="/">
+          MyDevfolio Logo
+        </u-link>
+      </div>
+      <div>
+        <SignedOut>
+          <u-link v-if="!hideSignInButton(route)" class="text-gray-50" to="/sign-in">
+            Iniciar sesión
+          </u-link>
+        </SignedOut>
+        <SignedIn>
+          <user-button show-name />
+        </SignedIn>
+      </div>
     </header>
 
     <slot />
-  </div>
+  </section>
 </template>
