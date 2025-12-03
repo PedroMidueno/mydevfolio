7a22c6882940220526ebf795590ed404c33c8cf4
PedroMidueno <pedromidueno.dev@gmail.com>
2025-12-02 18:00:38 -0600

    config: add lang attribute to whole document

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ import { dark } from "@clerk/themes";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-13',
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'es'
+      }
+    }
+  },
   runtimeConfig: {
     public: {
       bucketPrefixUrl: process.env.R2_BUCKET_PUBLIC_URL
