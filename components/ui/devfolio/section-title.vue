ee22361dfc3831f77f68dfa031c491e4edfb3a93
PedroMidueno <pedromidueno.dev@gmail.com>
2025-12-04 13:40:36 -0600

    feat: add ui components to portfolio page

---
+++ components/ui/devfolio/section-title.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { motion } from 'motion-v'
+
+interface Props {
+  text: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <motion.h2
+    class="text-2xl font-semibold md:font-bold"
+    :initial="{
+      scale: 0.9,
+      filter: 'blur(5px)',
+      opacity: 0
+    }"
+    :while-in-view="{
+      scale: 1,
+      filter: 'blur(0px)',
+      opacity: 1
+    }"
+    :in-view-options="{ once: true }"
+    :transition="{
+      delay: 0.1,
+      duration: 0.6,
+      ease: 'easeOut'
+    }"
+  >
+    {{ props.text }}
+  </motion.h2>
+</template>
---
+++ components/ui/devfolio/tech-skill.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  name: string
+  logo: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <u-tooltip :text="props.name">
+    <picture>
+      <img
+        class="w-full h-full object-contain"
+        :src="props.logo"
+        :alt="`Logotipo de ${props.name}`"
+      />
+    </picture>
+  </u-tooltip>
+</template>
