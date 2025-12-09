20f96dd93a351d969d1b548e4d4f169b45d6f547
PedroMidueno <pedromidueno.dev@gmail.com>
2025-12-09 13:42:09 -0600

    feat: add footer to devfolio page

--- components/devfolio/footer.vue
+++ components/devfolio/footer.vue
@@ -1,7 +1,8 @@
 <template>
-  <footer class="pt-4 w-full">
-    <div class="devfolio-container flex justify-center items-center">
-      Creado con MyDevfolio
-    </div>
+  <footer class="devfolio-container border-x border-slate-800 pt-16 pb-16 sm:pb-4 lg:pb-2 flex justify-center items-end">
+    <p class="text-xs text-gray-400">
+      <!-- TODO: Agregar url -->
+      Creado con <a href="#" class="text-blue-200">MyDevfolio</a>
+    </p>
   </footer>
 </template>
